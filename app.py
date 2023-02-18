e753ce8997ea29be00dda0ae6a54007b4fd97f61
Nagasai9299 <yanalasaichoudary1998@gmail.com>
2023-02-17 21:00:35 -0600

    added  routes for flask

--- app.py
+++ app.py
@@ -2,10 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.run('/')
+@app.route('/')
 def helloworld():
     print('helloworld')
     return  'Hello world'
+
+@app.route('/api')
+def api():
+    print('inside api')
+    return  'Inside api'
 
 if __name__ =='__main__':
     app.run()
